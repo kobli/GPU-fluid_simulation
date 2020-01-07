db60c62e2fce7637298ef5a1ec11d3718e860483
Ales Koblizek <koblizek.ales@seznam.cz>
2020-01-08 00:07:08 +0100

    add comments

--- sph.hpp
+++ sph.hpp
@@ -3,36 +3,49 @@
 #include "sphKernels.hpp"
 #include "bounds.hpp"
 
+/// Used for grid-based neighbour search
 struct ParticleRecord {
 	unsigned cellID;
 	unsigned particleID;
 };
 
+/// Used for grid-based neighbour search
 struct CellRecord {
 	unsigned firstParticleID;
 	unsigned particleN;
 };
 
+/// Holds SPH coefficients and other simulation parameters
 struct SPHconfig {
 	SPHconfig(unsigned _particleN, unsigned _subdivisionN): SubdivisionN{_subdivisionN}, particleN{_particleN}
 	{}
-	float Step; // [seconds]
-	float H;
-	float M;
-	float Rho0;
-	float K;
-	float Mu;
-	const unsigned SubdivisionN; // must be power of two
-	const unsigned particleN;
+	float Step; /// simulation step [seconds]
+	float H; /// kernel radius
+	float M; /// particle mass - is the same for all particles
+	float Rho0; /// fluid rest density - same for all particles
+	float K; /// pressure coefficient
+	float Mu; /// viscosity coefficient
+	const unsigned SubdivisionN; /// neighbour-search grid number of subdivisions in each dimension (must be power of two)
+	const unsigned particleN; /// numbe of particles
 };
 
+
+/* Base class for SPH implementation.
+ * Draws the particles.
+ */
 class SPH {
 	public:
 		SPH(SPHconfig &_config, Bounds& _b);
+
+		/// draw particles
 		void draw();
+		/// sets the buffer which contains the particle positions
+		/// \param numPositionComponents 	Number of components of the position vector. (GPU implementation uses 4 component vectors)
 		void setParticlePositionAttrBuffer(int numPositionComponents);
 
+		/// restart the simulation
 		virtual void reset() = 0;
+		/// single simulation step
 		virtual void update() = 0;
 
 	protected:
@@ -45,10 +58,11 @@ class SPH {
 
 
 	private:
+		/// initializes the sphere mesh used for particle visualization
 		void initSphereMesh();
 
 	public:
-		float frameTime;
+		float frameTime; /// the derived classes should write here the time required for simulation step
 
 	protected:
 		GLuint particlePositionBuff;
